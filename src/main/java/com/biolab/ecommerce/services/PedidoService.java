8145859f1a4aff6d48d4773a02bb978b46d9bbc6
ANDRÉ PASSOS DOS SANTOS <andre.santos161@senaisp.edu.br>
2026-08-26 11:12:32 -0300

    feat: corrigindo método 'criarPedido' em services.PedidoService

--- src/main/java/com/biolab/ecommerce/services/PedidoService.java
+++ src/main/java/com/biolab/ecommerce/services/PedidoService.java
@@ -2,10 +2,13 @@ package com.biolab.ecommerce.services;
 
 import com.biolab.ecommerce.DTOs.PedidoDTO;
 import com.biolab.ecommerce.entities.Pedido;
+import com.biolab.ecommerce.entities.StatusPedido;
 import com.biolab.ecommerce.entities.Usuario;
 import com.biolab.ecommerce.repositories.PedidoRepository;
 import com.biolab.ecommerce.repositories.UsuarioRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class PedidoService {
@@ -22,8 +25,8 @@ public class PedidoService {
         Usuario u = usuarioRepository.findById(dto.getIdCliente()).orElseThrow();
         Pedido p = new Pedido();
         p.setCliente(u);
-        p.setMomento(dto.getMomento());
-        p.setStatus(dto.getStatus());
+        p.setMomento(Instant.now());
+        p.setStatus(StatusPedido.AGUARDANDO_PAGAMENTO);
         pedidoRepository.save(p);
         return "Pedido deu green";
     }
